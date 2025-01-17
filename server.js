3db68a47bed188536d387486931c3f83f31ed5d1
Diego <you@example.com>
2025-01-17 14:48:30 -0300

    add static asset icon and improve logging for static paths

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 4200;
 
+console.log(
+  "Ruta estática:",
+  path.join(__dirname, "dist/blog-frontend/browser")
+);
+console.log(
+  "Ruta index.html:",
+  path.join(__dirname, "dist/blog-frontend/browser", "index.html")
+);
+
 // Sirve los archivos estáticos desde el directorio dist
 app.use(express.static(path.join(__dirname, "dist/blog-frontend/browser")));
 
 // Maneja todas las rutas no definidas y redirige a la aplicación Angular
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "dist/blog-frontend/browser", "index.html")
-  );
-  console.log(
-    "Ruta del directorio estático: ",
-    path.join(__dirname, "dist/blog-frontend/browser")
-  );
-  console.log(
-    "Ruta del archivo index.html: ",
     path.join(__dirname, "dist/blog-frontend/browser", "index.html")
   );
 });
